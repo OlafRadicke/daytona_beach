34aaf9a0520bd1b22e2bcd37f783eb560053aa02
Olaf Radicke <briefkasten@olaf-radicke.de>
2025-03-01 15:17:32 +0100

    Remove duplicate code.

--- scripts/ci-install.sh
+++ scripts/ci-install.sh
@@ -8,5 +8,5 @@ kubectx atlantic-ocean
 kubectl get node
 
 kubectl apply --wait=true -n argo -f ./manifest/workflow/01_befor
-sleep 2
+# sleep 2
 kubectl apply --wait=true -n argo -f ./manifest/workflow/05_after
